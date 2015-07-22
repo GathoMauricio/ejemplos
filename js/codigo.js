45edd3483198fc3a1f7c44c488b0ba44e2436e8a
GathoMauricio <mauricio2769@gmail.com>
2015-07-22 01:15:46 -0500

    alerts

--- js/codigo.js
+++ js/codigo.js
@@ -67,3 +67,23 @@ function clearDato()
 {
 	 window.localStorage.clear();
 }
+
+function showAlert() {
+        navigator.notification.alert(
+            'You are the winner!',  // message
+            'Game Over',            // title
+            'Done'                  // buttonName
+        );
+    }
+
+    // Beep three times
+    //
+    function playBeep() {
+        navigator.notification.beep(3);
+    }
+
+    // Vibrate for 2 seconds
+    //
+    function vibrate() {
+        navigator.notification.vibrate(2000);
+    }
