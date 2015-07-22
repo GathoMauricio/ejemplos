525266a77b82242599b5c8e89f46dfcc8ca87cc3
GathoMauricio <mauricio2769@gmail.com>
2015-07-21 22:39:44 -0500

    data

--- js/codigo.js
+++ js/codigo.js
@@ -12,12 +12,12 @@ var map;
 
 function initialize(position) { 
 $("#map-canvas").css("width","100%");
-$("#map-canvas").css("height","200px");
+$("#map-canvas").css("height","300px");
 
 myLatlng = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
 
 var mapOptions = { 
-zoom: 18, 
+zoom: 10, 
 center: myLatlng 
 }; 
 map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions); 
@@ -35,11 +35,11 @@ function onBodyLoad(){
                      
 function onError(error) { 
 $("#map-canvas").css("width","100%");
-$("#map-canvas").css("height","200px");
+$("#map-canvas").css("height","300px");
 myLatlng = new google.maps.LatLng(-34.397, 150.644);
 
 var mapOptions = { 
-zoom: 18, 
+zoom: 10, 
 center: myLatlng 
 }; 
 map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
