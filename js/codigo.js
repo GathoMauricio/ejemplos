e52796de92b6a027fc952955a4c88f01982df8d0
GathoMauricio <mauricio2769@gmail.com>
2015-07-21 21:16:40 -0500

    c

--- js/codigo.js
+++ js/codigo.js
@@ -1,7 +1,7 @@
 function init()
 {
 	$("div:jqmData(role='panel')").css('margin-top',  ($("div:jqmData(role='header')").height()));
-	initialize();
+	
 }
 var map; 
 
@@ -19,6 +19,7 @@ map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
 google.maps.event.addDomListener(window, 'load', initialize);
 function onBodyLoad(){
 navigator.geolocation.getCurrentPosition(onSuccess, onError); 
+initialize(position.coords.latitude,position.coords.longitude); 
 }
 function onDeviceReady() { 
 navigator.geolocation.getCurrentPosition(onSuccess, onError); 
