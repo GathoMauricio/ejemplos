d132e27cda84deeced05b0c39417bbe2a246e85f
GathoMauricio <mauricio2769@gmail.com>
2015-07-21 21:20:58 -0500

    success

--- js/codigo.js
+++ js/codigo.js
@@ -19,7 +19,7 @@ map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
 google.maps.event.addDomListener(window, 'load', initialize);
 function onBodyLoad(){
 navigator.geolocation.getCurrentPosition(onSuccess, onError); 
-initialize(position.coords.latitude,position.coords.longitude); 
+
 }
 function onDeviceReady() { 
 navigator.geolocation.getCurrentPosition(onSuccess, onError); 
@@ -27,6 +27,7 @@ navigator.geolocation.getCurrentPosition(onSuccess, onError);
              
 function onSuccess(position) { 
 initialize(position.coords.latitude,position.coords.longitude); 
+alert(position.coords.latitude+" / "+position.coords.longitude);
 } 
                        
 function onError(error) { 
