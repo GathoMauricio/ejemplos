f999dda6ecb80de82e63b8636b7b38c73eb26fc3
GathoMauricio <mauricio2769@gmail.com>
2015-07-21 22:30:46 -0500

    menu 1 ok

--- js/codigo.js
+++ js/codigo.js
@@ -3,16 +3,21 @@ function init()
 	$("div:jqmData(role='panel')").css('margin-top',  ($("div:jqmData(role='header')").height()));
 	
 }
+function miPocision()
+{
+	$("#contenedor").load("mapa.html");
+	navigator.geolocation.getCurrentPosition(initialize, onError); 
+}
 var map; 
 
 function initialize(position) { 
 $("#map-canvas").css("width","100%");
-$("#map-canvas").css("height","120px");
+$("#map-canvas").css("height","200px");
 
 myLatlng = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
 
 var mapOptions = { 
-zoom: 8, 
+zoom: 18, 
 center: myLatlng 
 }; 
 map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions); 
@@ -20,16 +25,16 @@ map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
 } 
 
 function onBodyLoad(){
-navigator.geolocation.getCurrentPosition(initialize, onError); 
+
 }
                      
 function onError(error) { 
 $("#map-canvas").css("width","100%");
-$("#map-canvas").css("height","120px");
+$("#map-canvas").css("height","200px");
 myLatlng = new google.maps.LatLng(-34.397, 150.644);
 
 var mapOptions = { 
-zoom: 8, 
+zoom: 18, 
 center: myLatlng 
 }; 
 map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
